be621c7d903a9292884621950b7fa1929f8ecc18
duyguerogluu <erogluduyguduygu@gmail.com>
2024-12-28 23:10:37 +0300

    footer and header created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,19 @@
+import {Inter} from '@next/font/google';
+
 import '@/styles/reset.css';
 import '@/styles/global.css';
+import '@/components/header';
+import '@/components/footer';
 
-
+const interFontFamily = Inter({ subsets: ["latin"]});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={interFontFamily.className}>
       <body className='container'>
-        {children}
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
