6b255eee54488b15a20228bf98ec1289e251efed
duyguerogluu <erogluduyguduygu@gmail.com>
2024-12-29 20:31:12 +0300

    categories added

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Movies from '@/mocks/movies.json';
+import Genres from '@/mocks/genres.json';
 import  { FeaturedMovie }  from '@/components/featured_movie';
+import { Categories } from '@/components/categories';
 
 function HomeContainer(){
-    return <FeaturedMovie movie={Movies.results[0]} />
+    return (
+        <div>
+            <FeaturedMovie movie={Movies.results[0]} />
+            <Categories categories={Genres.genres.slice(0, 6)}/>
+       </div>
+       
+   ); 
 }
 
 export default HomeContainer;
