c85d4af5368b140775e3108f898d0cd424c11f13
duyguerogluu <erogluduyguduygu@gmail.com>
2024-12-30 10:12:46 +0300

    popular films created

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -3,13 +3,15 @@ import Movies from '@/mocks/movies.json';
 import Genres from '@/mocks/genres.json';
 import  { FeaturedMovie }  from '@/components/featured_movie';
 import { Categories } from '@/components/categories';
+import { MoviesSection } from '@/components/movies_section';
 
 function HomeContainer(){
     return (
         <div>
             <FeaturedMovie movie={Movies.results[0]} />
             <Categories categories={Genres.genres.slice(0, 6)}/>
-       </div>
+            <MoviesSection title="Popüler Filmler" movies={Movies.results.slice(0,6)} /> 
+        </div>
        
    ); 
 }
